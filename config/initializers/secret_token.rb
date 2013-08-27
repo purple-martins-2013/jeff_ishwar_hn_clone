c01f5385d02e0919ad22832b6b1dbcd7ba0f88c0
Apprentice <apprentice@dbc21.local>
2013-08-26 17:19:54 -0700

    initial commit. setup rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JeffIshwarHn::Application.config.secret_key_base = 'cb6d7b31e56c24a2a3dc46eba60745d505be6652678921feb144fdcc5b4fb163008767941d8e391ab9438c28b381690878d44fe0e25b8dbba07565ea5a911f0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JeffIshwarHn::Application.config.session_store :cookie_store, key: '_jeff_ishwar_hn_session'
